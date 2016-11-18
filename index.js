8b211931601260185602147ed5a1ccaf5d4db229
stormslowly@gmail.com <stormslowly@gmail.com>
2016-11-18 22:05:38 +0800

    🚧      init the work

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var request  = require('superagent')
+var co = require('co')
+
+co(function*(){
+    var res = yield request.get('https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
+    var gitEmojis = JSON.parse(res.text).gitmojis
+
+    gitEmojis.forEach(function(gm){
+        console.log('#'+gm.emoji +'     '+gm.description)
+    })
+
+})
+.catch(function(error){
+    console.log(error,error.stack)
+    process.exit(-1)
+})
+.then(function(){
+    process.exit(0)
+})
+
